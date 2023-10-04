75ce9bbe33515f616947ae5916c3c5b1b07f5dfd
melearn2 <146975186+melearn2@users.noreply.github.com>
2023-10-04 23:28:10 +0530

    Update streamlit_app.py
    worked on quote error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom"'"s New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Oatmeal with warm milk')
 streamlit.text('🍞🐔 Bread & Omlette')
