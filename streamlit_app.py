5a7216905e7d2c6db6083a38035f6d8d436e7538
melearn2 <146975186+melearn2@users.noreply.github.com>
2023-10-04 23:25:35 +0530

    Update streamlit_app.py
    updated title
    added emojis to items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Oatmeal with warm milk')
-streamlit.text('Bread & Omlette')
-streamlit.text('Paratha & Tea')
+streamlit.title('My Mom's New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Oatmeal with warm milk')
+streamlit.text('🍞🐔 Bread & Omlette')
+streamlit.text('🥗 Veg Paratha & Tea')
