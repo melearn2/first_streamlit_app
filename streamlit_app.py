5face1b3a7ce8d092c2f2df689c7f3b4ee7382f7
melearn2 <146975186+melearn2@users.noreply.github.com>
2023-10-04 23:19:43 +0530

    Update streamlit_app.py
    added breakfast menu header & text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Oatmeal with warm milk')
+streamlit.text('Bread & Omlette')
+streamlit.text('Paratha & Tea')
