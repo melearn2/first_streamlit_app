c6cf10d5767d955716dbb14edc8d2b97963be6e5
melearn2 <146975186+melearn2@users.noreply.github.com>
2023-10-05 23:32:44 +0530

    Update streamlit_app.py
    pandas imported
    aws csv file data stored in dataframe & opened in streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Oatmeal with warm milk')
 streamlit.text('🍞🐔 Bread & Omlette')
 streamlit.text('🥗 Veg Paratha & Tea')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
