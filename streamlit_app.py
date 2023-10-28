ba78d27d4bef5a26d428bcac9cd52a5126958d97
melearn2 <146975186+melearn2@users.noreply.github.com>
2023-10-28 19:29:58 +0530

    Update streamlit_app.py
    added FruitVicy API section

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,9 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+#new section to display FruitVicy API response
+import requests
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
