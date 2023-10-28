13b2505bb3f091ec21328125608ab60dd8c04d7b
melearn2 <146975186+melearn2@users.noreply.github.com>
2023-10-29 00:39:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display FruitVicy API response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple', 'Cherry')
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # get response in variable to write in tabular format
