8b33a646abd39edb6c9764a8b2ed4d4dc31dc0a0
melearn2 <146975186+melearn2@users.noreply.github.com>
2023-10-28 20:39:34 +0530

    Update streamlit_app.py
    trying to eliminate error for response

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # get response in variable to write in tabular format
+streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write response in tabular format
 streamlit.dataframe(fruityvice_normalized)
