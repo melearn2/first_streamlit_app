10d9a06f413180014bbcf6ee8495fb5ad971ed1e
melearn2 <146975186+melearn2@users.noreply.github.com>
2023-10-28 20:26:42 +0530

    Update streamlit_app.py
    edited name spelling mistake in fruityvice_response

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
 streamlit.write('The user entered ', fruit_choice)
-fruitvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # get response in variable to write in tabular format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write response in tabular format
