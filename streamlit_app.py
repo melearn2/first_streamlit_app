6349cbd53f3603418adbd1640de851a704119d0c
melearn2 <146975186+melearn2@users.noreply.github.com>
2023-10-29 02:19:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,10 +49,23 @@ if streamlit.button('Get fruit list'):
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 
+#allow end user to add fruit of choice to snowflake table
+def insert_row_snowflake(new_fruit):
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+      return 'Thanks for adding ' + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    function_return = insert_row_snowflake(add_my_fruit)
+    streamlit.text(function_return)
+
+
 #stop running below steps till we troubleshoot
 streamlit.stop()
 #2nd option for fruit choice
-fruit_choice2 = streamlit.text_input('What fruit would you like information about?','Cherry')
+fruit_choice2 = 
 streamlit.write('The user entered ', fruit_choice2)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice2)
 # get response in variable to write in tabular format
@@ -60,6 +73,3 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write response in tabular format
 streamlit.dataframe(fruityvice_normalized)
-
-streamlit.write('Thanks for adding ', fruit_choice , fruit_choice2)
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
