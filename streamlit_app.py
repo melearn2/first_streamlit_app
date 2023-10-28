214540444604697be71bbb80ec3a581a4cea41a7
melearn2 <146975186+melearn2@users.noreply.github.com>
2023-10-29 01:40:21 +0530

    Update streamlit_app.py
    try except block added

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,20 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display FruitVicy API response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-# get response in variable to write in tabular format
-#streamlit.text(fruityvice_response.json())
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write response in tabular format
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if no fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    # get response in variable to write in tabular format
+    #streamlit.text(fruityvice_response.json())
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write response in tabular format
+    streamlit.dataframe(fruityvice_normalized)
+except:
+  URLError as e:
+  streamlit.error()
 #stop running below steps till we troubleshoot
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
