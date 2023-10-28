9dba7b93964d14fd3b408a32325b56ac5e02b305
melearn2 <146975186+melearn2@users.noreply.github.com>
2023-10-28 20:03:56 +0530

    Update streamlit_app.py
    input choice added

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 streamlit.header("Fruityvice Fruit Advice2!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
